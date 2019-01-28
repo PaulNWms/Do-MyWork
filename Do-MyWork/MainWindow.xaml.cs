31621016f04b7100e692883260a6ecc35aae344d
Paul Williams <paulwilliams_redmond@hotmail.com>
2019-01-27 18:31:37 -0800

    feat: FileSystemWatcher on AppConfig

--- Do-MyWork/MainWindow.xaml.cs
+++ Do-MyWork/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Threading.Tasks;
 using System.Windows;
 namespace Do_MyWork
 {
@@ -10,12 +13,37 @@ namespace Do_MyWork
         public MainWindow()
         {
             InitializeComponent();
+            LoadAppSettings();
+
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Path = ".";
+            watcher.NotifyFilter = NotifyFilters.LastWrite;
+            watcher.Filter = "Do-MyWork.exe.config";
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            Action action = ReloadAppSettings;
+            Dispatcher.BeginInvoke(action);
+        }
+
+        private void LoadAppSettings()
+        {
             var appSettings = ConfigurationManager.AppSettings;
             string editor = appSettings.Get("Editor");
             var eventHandlers = new MyEventHandlers(editor, tree);
             TreeBuilder treeBuilder = new TreeBuilder(eventHandlers, this.tree);
             treeBuilder.BuildTree();
             tree.Focus();
+        }
+
+        private void ReloadAppSettings()
+        {
+            ConfigurationManager.RefreshSection("appSettings");
+            ConfigurationManager.RefreshSection("TreeContents");
+            LoadAppSettings();
         }
     }
 }
