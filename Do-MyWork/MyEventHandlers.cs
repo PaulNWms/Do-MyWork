4313bf12b56f65c936805317c97b35b279181b02
Paul Williams <paulwilliams_redmond@hotmail.com>
2019-01-27 23:10:53 -0800

    feat: Files and dirs

--- Do-MyWork/MyEventHandlers.cs
+++ Do-MyWork/MyEventHandlers.cs
@@ -8,6 +8,8 @@ using System.Windows.Media.Media3D;
 
 namespace Do_MyWork
 {
+    internal enum TreeNodeType { Files, Dirs }
+
     class MyEventHandlers
     {
         public string Editor { get; private set; }
@@ -33,13 +35,94 @@ namespace Do_MyWork
                     Process.Start(this.Editor, tag);
                     break;
 
-                case "Open location":
+                case "Open Folder":
                     Process.Start("explorer.exe", Path.GetDirectoryName(tag));
                     break;
 
                 case "Open URL":
                     Process.Start(tag);
                     break;
+            }
+        }
+
+        public void FilesItem_Expanded(object sender, RoutedEventArgs e)
+        {
+            FileSystemItem_Expanded(TreeNodeType.Files, sender, e);
+        }
+
+        public void DirsItem_Expanded(object sender, RoutedEventArgs e)
+        {
+            FileSystemItem_Expanded(TreeNodeType.Dirs, sender, e);
+        }
+
+        public void FileSystemItem_Expanded(TreeNodeType treeNodeType, object sender, RoutedEventArgs e)
+        {
+            TreeViewItem treeViewItem = sender as TreeViewItem;
+            string pathFilter = treeViewItem.Tag.ToString();
+            string path = null;
+            string filter = null;
+
+            if (TryGetPathFilter(pathFilter, out path, out filter))
+            {
+                string[] children = null;
+
+                switch (treeNodeType)
+                {
+                    case TreeNodeType.Dirs:
+                        children = Directory.GetDirectories(path, filter);
+                        break;
+
+                    case TreeNodeType.Files:
+                    default:
+                        children = Directory.GetFiles(path, filter);
+                        break;
+                }
+
+                RemovePlaceholder(treeViewItem.Items);
+
+                foreach (string child in children)
+                {
+                    TreeViewItem item = new TreeViewItem();
+                    item.Header = child;
+                    treeViewItem.Items.Add(item);
+                }
+            }
+        }
+        private bool TryGetPathFilter(string pathFilter, out string path, out string filter)
+        {
+            int idx = pathFilter.LastIndexOf('\\');
+
+            if (idx >= 0)
+            {
+                path = pathFilter.Substring(0, idx);
+                filter = pathFilter.Substring(idx + 1);
+                return true;
+            }
+            else
+            {
+                path = null;
+                filter = null;
+                return false;
+            }
+        }
+
+        private void RemovePlaceholder(ItemCollection items)
+        {
+            int idx = -1;
+
+            for (int i = 0; i < items.Count; i++)
+            {
+                TreeViewItem item = items[i] as TreeViewItem;
+
+                if (item.Header.ToString() == ".")
+                {
+                    idx = i;
+                }
+            }
+
+            if (idx >= 0)
+            {
+                items.RemoveAt(idx);
             }
         }
 
--- Do-MyWork/TreeBuilder.cs
+++ Do-MyWork/TreeBuilder.cs
@@ -23,40 +23,7 @@ namespace Do_MyWork
                 XmlNode treeContents = (XmlNode)ConfigurationManager.GetSection("TreeContents");
                 this.Tree.Items.Clear();
                 this.Tree.BeginInit();
-
-                foreach (XmlNode xmlNode in treeContents.ChildNodes)
-                {
-                    TreeViewItem item = new TreeViewItem();
-                    item.Header = xmlNode.Attributes["name"].Value;
-                    this.Tree.Items.Add(item);
-
-                    if (xmlNode.Attributes["file"] != null)
-                    {
-                        item.Tag = xmlNode.Attributes["file"].Value;
-                        item.ContextMenu = new ContextMenu();
-                        MenuItem menuItem = new MenuItem();
-                        menuItem.Header = "Edit";
-                        menuItem.Click += this.MyEventHandlers.MenuItem_Click;
-                        item.ContextMenu.Items.Add(menuItem);
-                        menuItem = new MenuItem();
-                        menuItem.Header = "Open location";
-                        menuItem.Click += this.MyEventHandlers.MenuItem_Click;
-                        item.ContextMenu.Items.Add(menuItem);
-                    }
-
-                    if (xmlNode.Attributes["url"] != null)
-                    {
-                        item.Tag = xmlNode.Attributes["url"].Value;
-                        item.ContextMenu = new ContextMenu();
-                        MenuItem menuItem = new MenuItem();
-                        menuItem.Header = "Open URL";
-                        menuItem.Click += this.MyEventHandlers.MenuItem_Click;
-                        item.ContextMenu.Items.Add(menuItem);
-                    }
-
-                    AddChildNodes(xmlNode, item);
-                }
-
+                AddChildNodes(treeContents, this.Tree.Items);
                 this.Tree.EndInit();
             }
             catch (ConfigurationErrorsException ex)
@@ -65,14 +32,57 @@ namespace Do_MyWork
             }
         }
 
-        private void AddChildNodes(XmlNode parentXmlNode, TreeViewItem parentTreeNode)
+        private void AddChildNodes(XmlNode parentXmlNode, ItemCollection items)
         {
             foreach (XmlNode xmlNode in parentXmlNode.ChildNodes)
             {
                 TreeViewItem item = new TreeViewItem();
                 item.Header = xmlNode.Attributes["name"].Value;
-                parentTreeNode.Items.Add(item);
-                AddChildNodes(xmlNode, item);
+
+                if (xmlNode.Attributes["file"] != null)
+                {
+                    item.Tag = xmlNode.Attributes["file"].Value;
+                    item.ContextMenu = new ContextMenu();
+                    MenuItem menuItem = new MenuItem();
+                    menuItem.Header = "Edit";
+                    menuItem.Click += this.MyEventHandlers.MenuItem_Click;
+                    item.ContextMenu.Items.Add(menuItem);
+                    menuItem = new MenuItem();
+                    menuItem.Header = "Open Folder";
+                    menuItem.Click += this.MyEventHandlers.MenuItem_Click;
+                    item.ContextMenu.Items.Add(menuItem);
+                }
+
+                if (xmlNode.Attributes["url"] != null)
+                {
+                    item.Tag = xmlNode.Attributes["url"].Value;
+                    item.ContextMenu = new ContextMenu();
+                    MenuItem menuItem = new MenuItem();
+                    menuItem.Header = "Open URL";
+                    menuItem.Click += this.MyEventHandlers.MenuItem_Click;
+                    item.ContextMenu.Items.Add(menuItem);
+                }
+
+                if (xmlNode.Attributes["files"] != null)
+                {
+                    item.Tag = xmlNode.Attributes["files"].Value;
+                    item.Expanded += this.MyEventHandlers.FilesItem_Expanded;
+                    TreeViewItem placeholder = new TreeViewItem();
+                    placeholder.Header = ".";
+                    item.Items.Add(placeholder);
+                }
+
+                if (xmlNode.Attributes["dirs"] != null)
+                {
+                    item.Tag = xmlNode.Attributes["dirs"].Value;
+                    item.Expanded += this.MyEventHandlers.DirsItem_Expanded;
+                    TreeViewItem placeholder = new TreeViewItem();
+                    placeholder.Header = ".";
+                    item.Items.Add(placeholder);
+                }
+
+                items.Add(item);
+                AddChildNodes(xmlNode, item.Items);
             }
         }
     }
